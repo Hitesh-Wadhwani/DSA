37d74cca5fe90d7f90bd0a055b4449616640b0b9
Hitesh-Wadhwani <hiteshwadwani555@gmail.com>
2025-08-13 11:11:09 +0530

    Proper File names

---
+++ basic_practice_problems/find-the-largest-element.js
@@ -0,0 +1,21 @@
+function findLargest(arr){
+    if(!arr || arr.length < 2){
+        console.log("-1")
+    }else{
+        for(let i = 0; i<arr.length; i++){
+            if(arr[i] > largest){
+                largest = arr[i]
+            }
+        }
+        return largest
+    }    
+}
+
+
+let arr = [222,66,654];
+let largest = -Infinity
+
+
+let res = findLargest(arr);
+
+console.log(res)
